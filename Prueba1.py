2c6041e89fd0120842256a6b4f7a1499ffd4ae82
Brayhan  Ortiz <brayhanortiz27@gmail.com>
2023-09-07 20:06:47 -0500

    first commit

---
+++ Prueba1.py
@@ -0,0 +1,15 @@
+
+
+
+print ("Digite un numero")
+num = int(input())
+if num ==100:
+        print ("The numbers is equals")
+else:
+        if num > 100:
+                print ("The number is greater than "+ str(100))
+        else: 
+                print ("Is not greater than "+ str(100)+" is leen")
+
+
+    
